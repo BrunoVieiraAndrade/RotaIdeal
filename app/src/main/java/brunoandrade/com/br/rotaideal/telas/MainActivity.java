cc61e3f9f405eba6fc10a4e84a6c8eea4f7d8b8d
brunovieirandrade <brunoandrade.inf@gmail.com>
2016-12-13 12:14:51 -0200

    Initial commit

---
+++ app/src/main/java/brunoandrade/com/br/rotaideal/telas/MainActivity.java
@@ -0,0 +1,15 @@
+package brunoandrade.com.br.rotaideal.telas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import brunoandrade.com.br.rotaideal.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/brunoandrade/com/br/rotaideal/telas/servico/Calculo.java
@@ -0,0 +1,35 @@
+package brunoandrade.com.br.rotaideal.telas.servico;
+
+/**
+ * Created by JeanCarlos and Bruno Vieira on 12/12/2016.
+ */
+
+public class Calculo {
+
+
+    public static double calcularICMS(double cidade){
+
+        return cidade * 60;
+    }
+
+    public static double calcularImposto(double icmsLugar){
+
+        return (icmsLugar * 0.01) * 60;
+    }
+
+    public static double calcularTotalProduto(double imposto, double valorProduto, double custoTransporte){
+
+        return (imposto * valorProduto) + custoTransporte;
+    }
+
+
+    public static double calcularTudo(double valorDoProduto, double valorFrete, int cidade){
+
+        double icms = calcularICMS(cidade);
+        double imposto = calcularImposto(icms);
+        double totalProduto = calcularTotalProduto(imposto, valorDoProduto, valorFrete);
+
+        return totalProduto;
+
+    }
+}
